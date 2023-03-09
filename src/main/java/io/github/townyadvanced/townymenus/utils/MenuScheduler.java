d337105bdaa4682ab1b4ed4a829b4579cb9f62f0
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2023-03-09 19:21:53 +0100

    Log exceptions thrown in async tasks

--- src/main/java/io/github/townyadvanced/townymenus/utils/MenuScheduler.java
+++ src/main/java/io/github/townyadvanced/townymenus/utils/MenuScheduler.java
@@ -1,11 +1,13 @@
 package io.github.townyadvanced.townymenus.utils;
 
+import io.github.townyadvanced.townymenus.TownyMenus;
 import org.bukkit.entity.HumanEntity;
 
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
 
 public class MenuScheduler {
     private static final Set<UUID> hasRunningTask = ConcurrentHashMap.newKeySet();
@@ -21,6 +23,14 @@ public class MenuScheduler {
 
         hasRunningTask.add(uuid);
 
-        CompletableFuture.runAsync(runnable).thenRun(() -> hasRunningTask.remove(uuid));
+        CompletableFuture.runAsync(runnable)
+                .exceptionally(throwable -> {
+                    if (throwable.getCause() != null)
+                        throwable = throwable.getCause();
+
+                    TownyMenus.getPlugin().getLogger().log(Level.WARNING, "Error occurred when executing an async task for " + uuid, throwable);
+                    return null;
+                })
+                .thenRun(() -> hasRunningTask.remove(uuid));
     }
 }
