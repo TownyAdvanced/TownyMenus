aded0f897759a03dd43faf5aeb54ce4491afb7e3
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2023-07-01 18:56:00 +0200

    Use paperlib to avoid snapshotting

--- src/main/java/io/github/townyadvanced/townymenus/listeners/InventoryListener.java
+++ src/main/java/io/github/townyadvanced/townymenus/listeners/InventoryListener.java
@@ -1,5 +1,6 @@
 package io.github.townyadvanced.townymenus.listeners;
 
+import com.palmergames.paperlib.PaperLib;
 import io.github.townyadvanced.townymenus.TownyMenus;
 import io.github.townyadvanced.townymenus.gui.MenuInventory;
 import io.github.townyadvanced.townymenus.gui.MenuItem;
@@ -25,7 +26,7 @@ public class InventoryListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(final InventoryClickEvent event) {
-        if (!(event.getInventory().getHolder() instanceof MenuInventory menu))
+        if (!(PaperLib.getHolder(event.getInventory(), false).getHolder() instanceof MenuInventory menu))
             return;
 
         event.setCancelled(true);
