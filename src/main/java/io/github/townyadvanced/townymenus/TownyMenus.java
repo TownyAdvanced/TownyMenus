0e28a5a9ae9730d229a8455b649dec4f4a755bf1
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-05-19 15:42:47 +0200

    Fix english translations of other plugins being overwritten on config reload

--- src/main/java/io/github/townyadvanced/townymenus/TownyMenus.java
+++ src/main/java/io/github/townyadvanced/townymenus/TownyMenus.java
@@ -25,6 +25,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class TownyMenus extends JavaPlugin implements Listener {
@@ -127,6 +129,9 @@ public class TownyMenus extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onTranslationLoad(final TranslationLoadEvent event) {
-		event.getAddedTranslations().putAll(loadTranslations().getTranslations());
+		for (final Map.Entry<String, Map<String, String>> languageEntry : loadTranslations().getTranslations().entrySet()) {
+			final Map<String, String> languageMap = event.getAddedTranslations().computeIfAbsent(languageEntry.getKey(), k -> new HashMap<>());
+			languageMap.putAll(languageEntry.getValue());
+		}
 	}
 }
